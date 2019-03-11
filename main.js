04e2504621967a2784b27875b5ee49d2e17bc488
Igor Pevchev <i.pevchev@viseven.com>
2019-03-11 16:28:15 +0200

    update(main): update main process

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+const axios = require('axios');
+const path = require('path');
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+  });
+  mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+  mainWindow.webContents.openDevTools();
+  mainWindow.show();
+  mainWindow.on('ready-to-show', () => mainWindow.show());
+});
+
+axios
+.get('http://resources.finance.ua/ru/public/currency-cash.json')
+.then(response => global.sharedObject = response);
